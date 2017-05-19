db01b5bff3bb9d3f1644aa87b9b4353964fecf9d
Angela Cristina Barnes <acbarnes@cloudemployee.co.uk>
2017-05-19 17:11:53 +0800

    initial commit

---
+++ CountryBucketList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CountryBucketList
+//
+//  Created by Diel Barnes on 19/05/2017.
+//  Copyright © 2017 Diel Barnes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CountryBucketList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CountryBucketList
+//
+//  Created by Diel Barnes on 19/05/2017.
+//  Copyright © 2017 Diel Barnes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
